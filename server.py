2c3a9adb336c2108d189dd6dced6c2cd1ca07c71
Angelo Saraceno <asara019@fiu.edu>
2016-12-03 13:27:33 -0500

    started writing some routes

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+import csv
+import json
+
+csvfilename = "testcsv.csv"
+jsonfilename = csvfilename.split(0)[1].json
+
+
 @app.route('/')
 def welcome():
     return "this be the server boi"
+
+@app.route('/events')
+def events():
